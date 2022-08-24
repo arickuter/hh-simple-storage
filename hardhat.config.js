e619404bb15d5e5b0d717a44372b8b79889d0252
Aric Kuter <arickuter99@gmail.com>
2022-08-24 12:39:11 +0200

    local persistent blockchain

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,6 +13,7 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     rinkeby: { url: RINKEBY_RPC_URL, accounts: [PRIVATE_KEY], chainId: 4 },
+    localhost: { url: "http://127.0.0.1:8545/", chainId: 31337 },
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
